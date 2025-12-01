381acfdbf5344944e650621fdb5633dbdf459e92
Doom <de.simone.dc@gmail.com>
2025-12-01 20:47:05 +0100

    Add StringX

---
+++ src/warmup2/StringX.java
@@ -0,0 +1,20 @@
+package warmup2;
+
+public class StringX {
+    public static void main(String[] args) {
+        System.out.println(stringX("xxHxix")); // "xHix"
+        System.out.println(stringX("abxxxcd")); // "abcd"
+        System.out.println(stringX("xabxxxcdx")); // "xabcdx"
+    }
+
+    public static String stringX(String str) {
+
+        if (str.length() < 2) return str;
+
+        String newStr = str.charAt(0)
+                + str.substring(1, str.length()-1).replace("x", "")
+                + str.charAt(str.length()-1);
+
+        return newStr;
+    }
+}
