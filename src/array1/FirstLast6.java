e8095dbe844c5528767be013983b859745356736
Doom <de.simone.dc@gmail.com>
2025-12-07 11:40:49 +0100

    Add FirstLast6

---
+++ src/array1/FirstLast6.java
@@ -0,0 +1,12 @@
+package array1;
+
+public class FirstLast6 {
+    public static void main(String[] args) {
+        System.out.println(firstLast6(new int[]{1, 2, 6})); // true
+        System.out.println(firstLast6(new int[]{6, 1, 2, 3})); // true
+        System.out.println(firstLast6(new int[]{13, 6, 1, 2, 3})); // false
+    }
+    public static boolean firstLast6(int[] nums) {
+        return nums[0] == 6 || nums[nums.length-1] == 6;
+    }
+}
